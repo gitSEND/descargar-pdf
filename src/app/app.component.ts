b5c440a7d2211f84d636cea50496447c0cdfd8be
Nelson D. Sanchez E <n.sanchezesparza@gmail.com>
2022-07-06 21:01:27 -0400

    feat: descarga de pdf
    Se implementa funcionalidad para descargar los pdfs de forma multiple en safari y chromium

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import pdf from './data/pdfs.json'
 import {FileInterface} from "./interface/file.interface";
-import {downloadPdf, requestPdf} from "./util/download-file";
+import {downloadPdf} from "./util/download-file";
 
 @Component({
   selector: 'app-root',
@@ -10,22 +10,17 @@ import {downloadPdf, requestPdf} from "./util/download-file";
 })
 export class AppComponent {
 
+  // @ts-ignore
   public list: FileInterface[] = pdf.data;
 
   constructor() {
     console.log('list of pdfs', this.list);
   }
 
-  downloadFiles() {
+  async downloadFiles() {
     console.log('Descargando documentos...')
-    this.list.forEach(file => {
-      console.log(file.title)
-      downloadPdf(file)
-    });
+    for (const item of this.list) {
+      await downloadPdf(item);
+    }
   }
-
-
-
-
-
 }
--- src/app/util/download-file.ts
+++ src/app/util/download-file.ts
@@ -1,54 +1,18 @@
-/**
- * Convert Base64 (dataURI) to blob with mimeType
- * @param {String} dataURI
- * @returns {Blob}
- */
 import {FileInterface} from "../interface/file.interface";
 
-const dataURItoBlob = (dataURI: any) => {
-  const byteString = window.atob(dataURI.split(',')[1])
-  const mimeString = dataURI
-    .split(',')[0]
-    .split(':')[1]
-    .split(';')[0]
-
-  // write the bytes of the string to an ArrayBuffer
-  const ab = new ArrayBuffer(byteString.length)
-  const ia = new Uint8Array(ab)
-  for (let i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i)
-  }
-
-  // write the ArrayBuffer to a blob, and you're done
-  return new Blob([ab], {type: mimeString})
-}
-
-
 /**
- * requestPdf function to return a PDF
- * Converts base64 string to blob and inits download
- * @param {FileInterface} file
+ * With this function you can download several files in your browser
+ * @param file {FileInterface}
  */
-export const requestPdf = (file: FileInterface) => {
-  const linkSource = `data:application/pdf;base64,${file.base64}`
-  const pdfBlob = dataURItoBlob(linkSource)
+export const downloadPdf = (file: FileInterface): Promise<boolean> => {
+  return new Promise((resolve) => {
+    const link = document.createElement('a');
+    link.download = `${file.title}.pdf`;
+    link.href = `data:${file.type};base64,` + file.base64;
+    setTimeout(() => {
+      link.click();
+      resolve(true);
+    }, 100)
+  })
 
-  const url = window.URL.createObjectURL(pdfBlob)
-  const link = document.createElement('a')
-  link.href = url
-  link.setAttribute('download', `${file.title}.pdf`)
-  document.body.appendChild(link)
-  link.click()
-  link.remove()
-}
-
-/**
- * Permite descargar uno a varios pdfs en los navegadores
- * @param file
- */
-export const downloadPdf = (file: FileInterface) =>{
-  const link = document.createElement('a');
-  link.download = file.title;
-  link.href = 'data:application/octet-stream;base64,' + file.base64;
-  link.click();
 }
