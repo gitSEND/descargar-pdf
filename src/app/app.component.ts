a542d790974f75018c7daf5b1c6a1c375e9e4fce
Nelson Dennis Sanchez Esparza <nelson.sanchez@externos.bci.cl>
2022-07-08 10:21:58 -0400

    feat: se agrega funcion de zipeado de documentos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import {Component} from '@angular/core';
-import pdf from './data/pdfs.json'
-import {FileInterface} from "./interface/file.interface";
-import {downloadPdf} from "./util/download-file";
+import { Component } from '@angular/core';
+import JSZip from 'jszip';
+import pdf from './data/pdfs.json';
+import { FileInterface } from './interface/file.interface';
+import { downloadPdf } from './util/download-file';
+// @ts-ignore
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   // @ts-ignore
   public list: FileInterface[] = pdf.data;
 
@@ -18,14 +20,44 @@ export class AppComponent {
   }
 
   async downloadFiles() {
-    console.log('Descargando documentos...')
+    console.log('Descargando documentos...');
     for (const item of this.list) {
       await downloadPdf(item);
     }
   }
 
   async downloadFiles2(file: FileInterface) {
-    console.log('Descargando documentos...')
-      await downloadPdf(file);
+    console.log('Descargando documentos...');
+    await downloadPdf(file);
+  }
+
+  async generateZip() {
+    console.log('Descargando documentos zipeados...');
+
+    const zip = new JSZip();
+    const pdf = zip.folder('documents');
+
+    for (const file of this.list) {
+      pdf!.file(`${file.title}.pdf`, file.base64, { base64: true });
+    }
+
+    const content = await pdf!.generateAsync({ type: 'blob' });
+    saveAs(content, 'documents.zip');
+  }
+
+  async generateZip2() {
+    const zip = new JSZip();
+    const pdf = zip.folder('documents');
+
+    for (const file of this.list) {
+      pdf!.file(`${file.title}.pdf`, file.base64, { base64: true });
+    }
+
+    const content = await pdf!.generateAsync({ type: 'base64' });
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = `data:application/pdf;base64,${content}`;
+    downloadLink.download = 'documents.zip';
+    downloadLink.click();
   }
 }
--- src/app/util/download-file.ts
+++ src/app/util/download-file.ts
@@ -1,14 +1,17 @@
 import {FileInterface} from "../interface/file.interface";
 
 /**
- * With this function you can download several files in your browser
+ * With this function you can download one or several files in your browser
+ * in the same time. Apply a delay for safari
  * @param file {FileInterface}
  */
 export const downloadPdf = (file: FileInterface): Promise<boolean> => {
   return new Promise((resolve) => {
     const link = document.createElement('a');
+    link.href = `data:application/pdf;base64,` + file.base64;
     link.download = `${file.title}.pdf`;
-    link.href = `data:${file.type};base64,` + file.base64;
+    // link.href = `data:${file.type};base64,` + file.base64;
+
     setTimeout(() => {
       link.click();
       resolve(true);
